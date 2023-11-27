bfb9fdc9279c80c85ffdaab476c864fca937f937
Kota Muranaka <e235751@ie.u-ryukyu.ac.jp>
2023-11-27 19:29:44 +0900

    report4 step1,2
    e

--- app/src/main/java/jp/ac/uryukyu/ie/e235751/LivingThing.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235751/LivingThing.java
@@ -39,14 +39,16 @@ public class LivingThing {
     }
 
     public void attack(LivingThing opponent){
-        int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n",name, opponent.getName(), damage);
-        wounded(damage);
+        if ( this.dead == false ){
+            int damage = (int)(Math.random() * this.attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", this.name, opponent.getName(), damage);
+            opponent.wounded(damage);
+        }
     }
 
     public void wounded(int damage){
-        hitPoint -= damage;
-        if( hitPoint < 0 ) {
+        this.hitPoint -= damage;
+        if( this.hitPoint < 0 ) {
             dead = true;
             System.out.printf("%sは倒れた。\n", name);
         }
