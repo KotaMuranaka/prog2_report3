751aee2f49c91208989cb2c38460a49a3973a870
Kota Muranaka <e235751@ie.u-ryukyu.ac.jp>
2023-11-27 19:17:39 +0900

    report4 step1

--- app/src/main/java/jp/ac/uryukyu/ie/e235751/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e235751/Enemy.java
@@ -8,11 +8,11 @@ package jp.ac.uryukyu.ie.e235751;
  *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
-public class Enemy {
-    private String name;
-    private int hitPoint;
-    private int attack;
-    private boolean dead;
+public class Enemy extends LivingThing {
+    //private String name;
+    //private int hitPoint;
+    //private int attack;
+    //private boolean dead;
 
     /**
      * コンストラクタ。名前、最大HP、攻撃力を指定する。
@@ -21,11 +21,12 @@ public class Enemy {
      * @param attack モンスターの攻撃力
      */
     public Enemy (String name, int maximumHP, int attack) {
-        this.name = name;
-        hitPoint = maximumHP;
-        this.attack = attack;
-        dead = false;
-        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+        super(name,maximumHP,attack);
+        //this.name = name;
+        //hitPoint = maximumHP;
+        //this.attack = attack;
+        //dead = false;
+        //System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
     /**
@@ -33,26 +34,26 @@ public class Enemy {
      * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
      * @param hero 攻撃対象
      */
-    public void attack(Hero hero){
+    /*public void attack(Hero hero){
         if(dead==false){
             int damage = (int)(Math.random() * attack);
         System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
         hero.wounded(damage);
         }
-    }
+    }*/
 
     /**
      * 自身へ攻撃されたときのダメージ処理をするメソッド。
      * 指定されたダメージを hitPoint から引き、死亡判定を行う。
      * @param damage 受けたダメージ
      */
-    public void wounded(int damage){
+    /*public void wounded(int damage){
         hitPoint -= damage;
         if( hitPoint < 0 ) {
             dead = true;
             System.out.printf("モンスター%sは倒れた。\n", name);
         }
-    }
+    }*/
 
     /**
      * 敵の生死状態をゲッターを使うことで取得する。
@@ -60,7 +61,7 @@ public class Enemy {
      */
 
     public boolean getDead(){
-        return this.dead;
+        return super.isDead();
     }
 
     /**
@@ -68,17 +69,17 @@ public class Enemy {
      * @param dead trueの場合、敵は死亡している。falseの場合は生存している。
      */
 
-    public void setDead(boolean dead){
+    /*public void setDead(boolean dead){
         this.dead=dead;
-    }
+    }*/
 
     /**
      * 敵の名前をゲッターを使うことで取得する。
      * @return 敵の名前。
      */
-    public String getName(){
+    /*public String getName(){
         return name;
-    }
+    }*/
 
     /**
      * 敵の名前をセッターを使うことで設定する。
@@ -86,8 +87,8 @@ public class Enemy {
      */
 
 
-    public void setName(String name){
+    /*public void setName(String name){
         this.name=name;
-    }
+    }*/
 
 }
